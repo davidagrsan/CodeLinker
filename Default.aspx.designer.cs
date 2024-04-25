67d71239ca96e95dcde5d5da06b3bbc2452a1e16
davidagrsan <davidagredanosanchez@gmail.com>
2024-04-25 13:13:27 +0200

    Añadida animación de aparición de elementos en css y arregladas otros estilos

--- Default.aspx.designer.cs
+++ Default.aspx.designer.cs
@@ -11,43 +11,7 @@ namespace CodeLinker
 {
 
 
-    public partial class Login
+    public partial class _Default
     {
-
-        /// <summary>
-        /// Control lblConnected.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblConnected;
-
-        /// <summary>
-        /// Control txtBoxUser.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtBoxUser;
-
-        /// <summary>
-        /// Control txtBoxPwd.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtBoxPwd;
-
-        /// <summary>
-        /// Control btnLogin.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnLogin;
     }
 }
