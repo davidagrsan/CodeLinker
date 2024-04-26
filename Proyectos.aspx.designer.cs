f8e27c4d4afa793061686b0497f54819604683eb
David <davidagredanosanchez@gmail.com>
2024-04-26 19:18:47 +0200

    Arregladas cositas

--- Proyectos.aspx.designer.cs
+++ Proyectos.aspx.designer.cs
@@ -87,13 +87,13 @@ namespace CodeLinker
         protected global::System.Web.UI.WebControls.CheckBox checkEnded;
 
         /// <summary>
-        /// Control lblcheckEnded.
+        /// Control lblCheckEnded.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblcheckEnded;
+        protected global::System.Web.UI.WebControls.Label lblCheckEnded;
 
         /// <summary>
         /// Control comboProgrammingLanguage.
